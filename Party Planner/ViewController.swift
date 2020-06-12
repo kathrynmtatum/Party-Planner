40603000844b2f44a8fd6d42dcad27ca75190e0a
Kathryn Tatum <tatumka@bc.edu>
2020-06-12 11:44:46 -0400

    Final Push

--- Party Planner/ViewController.swift
+++ Party Planner/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var partyItems = ["Potato Chips",
+    "Tortilla Chips",
+    "Salsa",
+    "Chili",
+    "Punch",
+    "Sudsy Beverages",
+    "Brownies",
+    "Cupcakes",
+    "Fruit salad",
+    "Ribs",
+    "Corn bread",
+    "Macaroni Salad",
+    "Sandwich Rolls",
+    "Roast Beef",
+    "Ham",
+    "Cheese",
+    "Mayo",
+    "Mustard",
+    "Hummus",
+    "Baby carrots",
+    "Celery",
+    "Veggie Dip",
+    "Napkins",
+    "Plates & Bowls",
+    "Forks and Knives",
+    "Cups"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return partyItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = partyItems[indexPath.row] //important line
+        return cell
+    }
+    
+}
